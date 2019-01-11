b87b551e2901c45ec17abbfd236579e61fec97b3
vhcsoft <vhcblog@gmail.com>
2019-01-11 18:07:05 +0100

    init project
    init project

---
+++ src/main/java/com/vhcsoft/randomdata/RandomDataApplication.java
@@ -0,0 +1,14 @@
+package com.vhcsoft.randomdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomDataApplication.class, args);
+	}
+
+}
+
